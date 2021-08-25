5f75e7ebbf9b6548f728ac4a27f0568fa2c7a59b
Soulsaver35815 <86435297+finnsaile@users.noreply.github.com>
2021-08-26 01:44:07 +0200

    Cleanup and finetuning
    Changed variable names according to style guide, cleaned up constructor and implemented slider logic for variable min and max values

--- headers/CSlider.hpp
+++ headers/CSlider.hpp
@@ -6,21 +6,30 @@
 class CSlider : public sf::Drawable
 {
     public:
+        //constructor
         CSlider(int*, int, int, std::string);
+        //move slider according to input coordinate
         void moveSlider(int);
+        //return slider bounds for activation
         sf::FloatRect getSliderBounds();
     private:
-        int* variablePointer;
-        int sliderMin;
-        int sliderMax;
-        int sliderPosX;
-        int sliderPosY;
-        sf::Texture sliderBarTexture;
-        sf::Texture sliderDotTexture;
-        sf::Texture sliderNameTexture;
-        sf::Sprite sliderBarSprite;
-        sf::Sprite sliderDotSprite;
-        sf::Sprite sliderNameSprite;
+        //initialises all slider sprites
+        void initSlider();
+
+        //values for calulating the correct positions and values
+        int* m_slider_value;
+        int m_slider_min = 0;
+        int m_slider_max = 100;
+        int m_slider_pos_x;
+        int m_slider_pos_y;
+
+        //textures and sprites
+        sf::Texture m_slider_bar_texture;
+        sf::Texture m_slider_dot_texture;
+        sf::Texture m_slider_name_texture;
+        sf::Sprite m_slider_bar_sprite;
+        sf::Sprite m_slider_dot_sprite;
+        sf::Sprite m_slider_name_sprite;
         
         virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
 };
--- src/CSlider.cpp
+++ src/CSlider.cpp
@@ -1,55 +1,87 @@
 #include "../headers/CSlider.hpp"
 #include <iostream>
 
-CSlider::CSlider(int* variableAdress, int posX, int posY, std::string sliderNamePath) : 
-sf::Drawable(),
-variablePointer(variableAdress),
-sliderPosX(posX),
-sliderPosY(posY),
-sliderMin(0),
-sliderMax(100)
-{
-    sliderNameTexture.loadFromFile(sliderNamePath);
-    sliderBarTexture.loadFromFile(dataPath + "Slider/SliderBar.png");
-    sliderDotTexture.loadFromFile(dataPath + "Slider/SliderDot.png");
-    sliderBarSprite.setTexture(sliderBarTexture);
-    sliderDotSprite.setTexture(sliderDotTexture);
-    sliderBarSprite.setOrigin(0/*int(sliderBarSprite.getGlobalBounds().width/2)*/, int(sliderBarSprite.getGlobalBounds().height/2));
-    sliderDotSprite.setOrigin(int(sliderDotSprite.getGlobalBounds().width/2), int(sliderDotSprite.getGlobalBounds().height/2));
-    sliderBarSprite.setPosition(sliderPosX - 125, sliderPosY);
-    sliderDotSprite.setPosition(sliderBarSprite.getPosition().x + (*variablePointer) * 2.5, sliderPosY);
-    sliderNameSprite.setTexture(sliderNameTexture);
-    sliderNameSprite.setOrigin(int(sliderNameSprite.getGlobalBounds().width) / 2, int(sliderNameSprite.getGlobalBounds().height) / 2);
-    sliderNameSprite.setPosition(sliderPosX, sliderPosY - 30);
+#define SLIDER_WIDTH 250
+//initialise all textures and sprites for the slider
+CSlider::CSlider(int* value_pointer, int pos_x, int pos_y, std::string sliderNamePath) : 
+m_slider_value(value_pointer),
+m_slider_pos_x(pos_x),
+m_slider_pos_y(pos_y)
+{   
+    //load texture for slider name
+    m_slider_name_texture.loadFromFile(sliderNamePath);
+
+    //check if value of slider is out of bounds, 
+    //if yes change value to upper or lower bound respectively 
+    if(*m_slider_value > m_slider_max)
+        *m_slider_value = m_slider_max;
+    else if(*m_slider_value < m_slider_min)
+        *m_slider_value = m_slider_min;    
+
+    initSlider(); 
 }
 
-void CSlider::moveSlider(int cordX)
-{
-    if(cordX >= sliderBarSprite.getPosition().x && cordX <= sliderBarSprite.getPosition().x + 250)
+//move slider to given x coordinate if the slider dot is still within the slider bounds
+void CSlider::moveSlider(int cord_x)
+{   
+    //if input coordinate is within the bounds of the slider coordinates move slider dot
+    if(cord_x >= m_slider_bar_sprite.getPosition().x && cord_x <= m_slider_bar_sprite.getPosition().x + SLIDER_WIDTH)
     {
-        sliderDotSprite.setPosition(cordX, sliderPosY);
-        *variablePointer = int((cordX - sliderBarSprite.getPosition().x) / 2.5);
+        //move slider dot to input x position and slider y position
+        m_slider_dot_sprite.setPosition(cord_x, m_slider_pos_y);
+        //set value which the slider regulates according to postion of the slider dot, min and max value of the slider
+        *m_slider_value = m_slider_min + static_cast<int>((cord_x - m_slider_bar_sprite.getPosition().x) / (static_cast<float>(SLIDER_WIDTH)/(m_slider_max - m_slider_min)));
+    }   
+    //if input cords are smaller than lower bound
+    else if(cord_x < m_slider_bar_sprite.getPosition().x)
+    {   
+        //set slider dot to lower bound position
+        m_slider_dot_sprite.setPosition(m_slider_pos_x - SLIDER_WIDTH/2, m_slider_pos_y);
+        //set slider value to min value
+        *m_slider_value = m_slider_min;
     }
-    else if(cordX < sliderBarSprite.getPosition().x)
-    {
-        sliderDotSprite.setPosition(sliderPosX - 125, sliderPosY);
-        *variablePointer = 0;
-    }
+    //if input cors are larger than upper bound position
     else
-    {
-        sliderDotSprite.setPosition(sliderPosX + 125, sliderPosY);
-        *variablePointer = 100;
+    {   
+        //set slider dot to upper bound position
+        m_slider_dot_sprite.setPosition(m_slider_pos_x + SLIDER_WIDTH/2, m_slider_pos_y);
+        //set slider value to max value
+        *m_slider_value = m_slider_max;
     }
 }
 
+//init position, origin and textures for slider components
+void CSlider::initSlider()
+{
+    m_slider_bar_texture.loadFromFile(dataPath + "Slider/SliderBar.png");
+    m_slider_dot_texture.loadFromFile(dataPath + "Slider/SliderDot.png");
+
+    m_slider_bar_sprite.setTexture(m_slider_bar_texture);
+    m_slider_dot_sprite.setTexture(m_slider_dot_texture);
+    m_slider_name_sprite.setTexture(m_slider_name_texture);
+
+    //origin of slider bar is on the lower bound in the center
+    m_slider_bar_sprite.setOrigin(0, int(m_slider_bar_sprite.getGlobalBounds().height/2));
+    m_slider_dot_sprite.setOrigin(int(m_slider_dot_sprite.getGlobalBounds().width/2), int(m_slider_dot_sprite.getGlobalBounds().height/2));
+    m_slider_name_sprite.setOrigin(int(m_slider_name_sprite.getGlobalBounds().width) / 2, int(m_slider_name_sprite.getGlobalBounds().height) / 2);
+
+    //position of slider bar is set according to slider location and slider bar origin
+    m_slider_bar_sprite.setPosition(m_slider_pos_x - SLIDER_WIDTH/2, m_slider_pos_y);
+    //slider dot x position is set using slider value and min value to determine the value, and slider width over slider value delta to determin the scale
+    m_slider_dot_sprite.setPosition(m_slider_bar_sprite.getPosition().x + (*m_slider_value - m_slider_min) * (static_cast<float>(SLIDER_WIDTH)/(m_slider_max - m_slider_min)), m_slider_pos_y);
+    m_slider_name_sprite.setPosition(m_slider_pos_x, m_slider_pos_y - 30); 
+}
+
+//get bounds of slider dot to check for activation
 sf::FloatRect CSlider::getSliderBounds()
 {
-    return(sliderDotSprite.getGlobalBounds());
+    return(m_slider_dot_sprite.getGlobalBounds());
 }
 
+//draws all slider components
 void CSlider::draw(sf::RenderTarget& target, sf::RenderStates states) const
 {
-    target.draw(sliderBarSprite);
-    target.draw(sliderDotSprite);
-    target.draw(sliderNameSprite);
+    target.draw(m_slider_bar_sprite);
+    target.draw(m_slider_dot_sprite);
+    target.draw(m_slider_name_sprite);
 }
