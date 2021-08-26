764bd62d4d4018cae26a094fb75ab1cee0e83496
Soulsaver35815 <86435297+finnsaile@users.noreply.github.com>
2021-08-27 00:21:06 +0200

    Added all resources for the game

--- headers/CResources.hpp
+++ headers/CResources.hpp
@@ -1,6 +1,6 @@
 #pragma once
 #include <SFML/Graphics.hpp>
-#include "dataPath.hpp"
+#include <SFML/Audio.hpp>
 
 class CResources 
 {
@@ -26,6 +26,46 @@ class CResources
         sf::Texture m_tail_a;
         sf::Texture m_tail_s;
         sf::Texture m_tail_d;
+
+        sf::Texture m_grid_texture_light;
+        sf::Texture m_grid_texture_dark;
+
+        sf::Texture m_slider_bar_texture;
+        sf::Texture m_slider_dot_texture;
+
+        sf::Texture m_difficulty_texture;
+        sf::Texture m_volume_texture;
+        sf::Texture m_difficulty_easy_texture;
+        sf::Texture m_difficulty_medium_texture;
+        sf::Texture m_difficulty_hard_texture;
+        sf::Texture m_difficulty_extreme_texture;
+        sf::Texture m_difficulty_impossible_texture;
+        sf::Texture m_menu_texture;
+
+        sf::Texture m_slider_music_volume;
+        sf::Texture m_slider_eat_volume;
+        sf::Texture m_slider_lost_volume;
+        sf::Texture m_slider_click_volume;
+
+        sf::Texture m_logo_texture;
+        sf::Texture m_play_texture;
+        sf::Texture m_quit_texture;
+        sf::Texture m_settings_texture;
+
+        sf::Texture m_game_menu_break_menu;
+        sf::Texture m_game_menu_resume;
+        sf::Texture m_game_menu_menu;
+        sf::Texture m_game_menu_game_over;
+        sf::Texture m_game_menu_play_again;
+
+        sf::Texture m_food_texture;
+        
+        sf::SoundBuffer m_click_buffer;
+        sf::SoundBuffer m_crunch_buffer;
+        sf::SoundBuffer m_game_over_buffer;
+        sf::Music m_game_music; 
+
+        sf::Font m_game_font;
 
     private:
         CResources();
--- src/CResources.cpp
+++ src/CResources.cpp
@@ -5,13 +5,15 @@
 CResources CResources::m_instance;
 
 CResources::CResources() :
-DATA_PATH("/Users/finnsaile/Documents/CPP/OwnProjekts/SFML/Snake/resources/")
+DATA_PATH("/Users/finnsaile/Documents/GitHub/Snake/resources/")
 {
+    //snake head tectures
     m_head_w.loadFromFile(DATA_PATH + "Snake/SnakeHeadW.png");
     m_head_a.loadFromFile(DATA_PATH + "Snake/SnakeHeadA.png");
     m_head_s.loadFromFile(DATA_PATH + "Snake/SnakeHeadS.png");
     m_head_d.loadFromFile(DATA_PATH + "Snake/SnakeHeadD.png");
 
+    //snake body textures
     m_body_ws.loadFromFile(DATA_PATH + "Snake/SnakeBodyWS.png");
     m_body_ad.loadFromFile(DATA_PATH + "Snake/SnakeBodyAD.png");
     m_body_aw.loadFromFile(DATA_PATH + "Snake/SnakeBodyAW.png");
@@ -19,10 +21,60 @@ DATA_PATH("/Users/finnsaile/Documents/CPP/OwnProjekts/SFML/Snake/resources/")
     m_body_as.loadFromFile(DATA_PATH + "Snake/SnakeBodyAS.png");
     m_body_ds.loadFromFile(DATA_PATH + "Snake/SnakeBodyDS.png");
 
+    //snake tail textures
     m_tail_w.loadFromFile(DATA_PATH + "Snake/SnakeTailW.png");
     m_tail_a.loadFromFile(DATA_PATH + "Snake/SnakeTailA.png");
     m_tail_s.loadFromFile(DATA_PATH + "Snake/SnakeTailS.png");
     m_tail_d.loadFromFile(DATA_PATH + "Snake/SnakeTailD.png");
+
+    //grid textures
+    m_grid_texture_light.loadFromFile(DATA_PATH + "Floor/GrassLight.png");
+    m_grid_texture_dark.loadFromFile(DATA_PATH + "Floor/GrassDark.png");
+
+    //slider textures
+    m_slider_bar_texture.loadFromFile(DATA_PATH + "Slider/SliderBar.png");
+    m_slider_dot_texture.loadFromFile(DATA_PATH + "Slider/SliderDot.png");
+
+    //settings textures
+    m_difficulty_easy_texture.loadFromFile(DATA_PATH + "Settings/Easy.png");
+    m_difficulty_medium_texture.loadFromFile(DATA_PATH + "Settings/Medium.png");
+    m_difficulty_hard_texture.loadFromFile(DATA_PATH + "Settings/Hard.png");
+    m_difficulty_extreme_texture.loadFromFile(DATA_PATH + "Settings/Extreme.png");
+    m_difficulty_impossible_texture.loadFromFile(DATA_PATH + "Settings/Impossible.png");
+    m_difficulty_texture.loadFromFile(DATA_PATH + "Settings/Difficulty.png");
+    m_volume_texture.loadFromFile(DATA_PATH + "Settings/Volume.png");
+    m_menu_texture.loadFromFile(DATA_PATH + "Settings/Menu.png");
+
+    //slider name textures
+    m_slider_music_volume.loadFromFile(DATA_PATH + "Slider/MusicVolume.png");
+    m_slider_eat_volume.loadFromFile(DATA_PATH + "Slider/EatVolume.png");
+    m_slider_lost_volume.loadFromFile(DATA_PATH + "Slider/LostVolume.png");
+    m_slider_click_volume.loadFromFile(DATA_PATH + "Slider/ClickVolume.png");
+
+    //main menu textures
+    m_logo_texture.loadFromFile(DATA_PATH + "Menu/SnakeLogo.png");
+    m_play_texture.loadFromFile(DATA_PATH + "Menu/Play.png");
+    m_quit_texture.loadFromFile(DATA_PATH + "Menu/Quit.png");
+    m_settings_texture.loadFromFile(DATA_PATH + "Menu/Settings.png");
+
+    //game menu textures
+    m_game_menu_break_menu.loadFromFile(DATA_PATH + "GameMenu/BreakMenu.png");
+    m_game_menu_resume.loadFromFile(DATA_PATH + "GameMenu/Resume.png");
+    m_game_menu_menu.loadFromFile(DATA_PATH + "GameMenu/Menu.png");
+    m_game_menu_game_over.loadFromFile(DATA_PATH + "GameMenu/GameOverMenu.png");
+    m_game_menu_play_again.loadFromFile(DATA_PATH + "GameMenu/PlayAgain.png");
+
+    //food textures
+    m_food_texture.loadFromFile(DATA_PATH + "Floor/Apple.png");
+
+    //sounds
+    m_click_buffer.loadFromFile(DATA_PATH + "Sounds/ClickSound.wav");
+    m_crunch_buffer.loadFromFile(DATA_PATH + "Sounds/Crunch.wav");
+    m_game_over_buffer.loadFromFile(DATA_PATH + "Sounds/GameOverSound.wav");
+    m_game_music.openFromFile(DATA_PATH + "Sounds/bgMusic.wav");
+
+    //fonts
+    m_game_font.loadFromFile(DATA_PATH + "Fonts/ARCADE_R.TTF");
 }
 
 CResources& CResources::getInstance()
