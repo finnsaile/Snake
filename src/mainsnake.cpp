d3e1f53309fe7390178276f4aa1d741cdaf07ec6
Soulsaver35815 <86435297+finnsaile@users.noreply.github.com>
2021-08-27 00:21:33 +0200

    Changed variable names

--- src/mainsnake.cpp
+++ src/mainsnake.cpp
@@ -6,26 +6,26 @@
 
 using namespace sf;
 
-
 int main(int argc, char* argv[])
 {
     //initialize time 
     srand(time(NULL));
 
     //create window with VSync
-    RenderWindow gamewindow(VideoMode(WIN_SIZE, WIN_SIZE), "Snake", Style::Titlebar | Style::Close);
-    gamewindow.setVerticalSyncEnabled(true);
-    gamewindow.setKeyRepeatEnabled(false); 
+    RenderWindow render_window(VideoMode(WIN_SIZE, WIN_SIZE), "Snake", Style::Titlebar | Style::Close);
+    render_window.setVerticalSyncEnabled(false);
+    render_window.setFramerateLimit(144);
+    render_window.setKeyRepeatEnabled(false); 
     //create window object
     CWindow window;
-    while(gamewindow.isOpen())
+    while(render_window.isOpen())
     {
         //game tick from window object
-        window.windowTick(gamewindow);
+        window.windowTick(render_window);
         
         //clear, draw and display window object
-        gamewindow.clear(Color (169,169,169));
-        gamewindow.draw(window);
-        gamewindow.display();
+        render_window.clear(Color (169,169,169));
+        render_window.draw(window);
+        render_window.display();
     }    
 }
