7e642b878234c922afcb3b4905a0ab97b9c2e80f
Soulsaver35815 <86435297+finnsaile@users.noreply.github.com>
2021-09-07 20:57:45 +0200

    Changed slider labels to strings

--- headers/CSlider.hpp
+++ headers/CSlider.hpp
@@ -11,15 +11,15 @@ class CSlider : public sf::Drawable
 {
     public:
         //constructor
-        CSlider(T*, int, int, sf::Texture&, int = 0, int = 100);
+        CSlider(T*, int, int, std::string, int = 0, int = 100);
         //move slider according to input coordinate
         bool moveSlider(int);
         //return slider bounds for activation
         sf::FloatRect getSliderBounds() const; 
-        void setSliderName(sf::Texture&);
+        void setSliderLabel(std::string);
     private:
         //initialises all slider sprites
-        void initSlider();
+        void initSlider(std::string);
 
         //values for calulating the correct positions and values
         T* m_slider_value;
@@ -30,25 +30,23 @@ class CSlider : public sf::Drawable
 
         CResources& m_resource;
         //textures and sprites
-        const sf::Texture* m_slider_name_texture;
 
         sf::Sprite m_slider_bar_sprite;
         sf::Sprite m_slider_dot_sprite;
-        sf::Sprite m_slider_name_sprite;
+        sf::Text m_slider_label_text;
         
         virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
 };
 
 //initialise all textures and sprites for the slider
 template<typename T>
-CSlider<T>::CSlider(T* value_pointer, int pos_x, int pos_y, sf::Texture& slider_name_texture, int slider_min, int slider_max) : 
+CSlider<T>::CSlider(T* value_pointer, int pos_x, int pos_y, std::string slider_label_string, int slider_min, int slider_max) : 
 m_slider_max(slider_max),
 m_slider_min(slider_min),
 m_slider_value(value_pointer),
 m_slider_pos_x(pos_x),
 m_slider_pos_y(pos_y),
-m_resource(CResources::getInstance()),
-m_slider_name_texture(&slider_name_texture)
+m_resource(CResources::getInstance())
 {   
     //check if value of slider is out of bounds, 
     //if yes change value to upper or lower bound respectively 
@@ -57,7 +55,7 @@ m_slider_name_texture(&slider_name_texture)
     else if(*m_slider_value < static_cast<T>(m_slider_min))
         *m_slider_value = static_cast<T>(m_slider_min);    
 
-    initSlider(); 
+    initSlider(slider_label_string); 
 }
 
 //move slider to given x coordinate if the slider dot is still within the slider bounds
@@ -101,22 +99,28 @@ bool CSlider<T>::moveSlider(int cord_x)
 
 //init position, origin and textures for slider components
 template<typename T>
-void CSlider<T>::initSlider()
+void CSlider<T>::initSlider(std::string slider_label_string)
 {
+    m_slider_label_text.setString(slider_label_string);
+    m_slider_label_text.setFont(m_resource.m_game_font);
+    m_slider_label_text.setCharacterSize(20);
+    m_slider_label_text.setOutlineThickness(1);
+    m_slider_label_text.setFillColor(sf::Color(140, 198, 63));
+    m_slider_label_text.setOutlineColor(sf::Color::Black);
+
     m_slider_bar_sprite.setTexture(m_resource.m_slider_bar_texture);
     m_slider_dot_sprite.setTexture(m_resource.m_slider_dot_texture);
-    m_slider_name_sprite.setTexture(*m_slider_name_texture);
 
     //origin of slider bar is on the lower bound in the center
     m_slider_bar_sprite.setOrigin(0, int(m_slider_bar_sprite.getGlobalBounds().height/2));
     m_slider_dot_sprite.setOrigin(int(m_slider_dot_sprite.getGlobalBounds().width/2), int(m_slider_dot_sprite.getGlobalBounds().height/2));
-    m_slider_name_sprite.setOrigin(int(m_slider_name_sprite.getGlobalBounds().width) / 2, int(m_slider_name_sprite.getGlobalBounds().height) / 2);
+    m_slider_label_text.setOrigin(int(m_slider_label_text.getGlobalBounds().width) / 2, int(m_slider_label_text.getGlobalBounds().height) / 2);
 
     //position of slider bar is set according to slider location and slider bar origin
     m_slider_bar_sprite.setPosition(m_slider_pos_x - SLIDER_WIDTH/2, m_slider_pos_y);
     //slider dot x position is set using slider value and min value to determine the value, and slider width over slider value delta to determin the scale
     m_slider_dot_sprite.setPosition(m_slider_bar_sprite.getPosition().x + (*m_slider_value - m_slider_min) * (static_cast<float>(SLIDER_WIDTH)/(m_slider_max - m_slider_min)), m_slider_pos_y);
-    m_slider_name_sprite.setPosition(m_slider_pos_x, m_slider_pos_y - 30); 
+    m_slider_label_text.setPosition(m_slider_pos_x, m_slider_pos_y - 30); 
 }
 
 //get bounds of slider dot to check for activation
@@ -128,14 +132,11 @@ sf::FloatRect CSlider<T>::getSliderBounds() const
 
 //change the texture of the slider name
 template<typename T>
-void CSlider<T>::setSliderName(sf::Texture& slider_name_texture)
+void CSlider<T>::setSliderLabel(std::string slider_label_string)
 {
-    m_slider_name_texture = &slider_name_texture;
-    sf::IntRect temp_int_rect(0, 0, m_slider_name_texture->getSize().x, m_slider_name_texture->getSize().y);
-    m_slider_name_sprite.setTextureRect(temp_int_rect);
-    m_slider_name_sprite.setTexture(*m_slider_name_texture);
-    m_slider_name_sprite.setOrigin(int(m_slider_name_sprite.getGlobalBounds().width) / 2, int(m_slider_name_sprite.getGlobalBounds().height) / 2);
-    m_slider_name_sprite.setPosition(m_slider_pos_x, m_slider_pos_y - 30); 
+    m_slider_label_text.setString(slider_label_string);
+    m_slider_label_text.setOrigin(int(m_slider_label_text.getGlobalBounds().width) / 2, int(m_slider_label_text.getGlobalBounds().height) / 2);
+    m_slider_label_text.setPosition(m_slider_pos_x, m_slider_pos_y - 30); 
 }
 
 //draws all slider components
@@ -144,5 +145,5 @@ void CSlider<T>::draw(sf::RenderTarget& target, sf::RenderStates states) const
 {
     target.draw(m_slider_bar_sprite);
     target.draw(m_slider_dot_sprite);
-    target.draw(m_slider_name_sprite);
+    target.draw(m_slider_label_text);
 }
